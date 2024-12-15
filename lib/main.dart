b554d8281d45dc123c79cd5c51e1a34bd11f07d7
Sainathhiwale <sainathhiwale2@gmail.com>
2024-12-15 23:39:56 +0530

    fat arrow for dashbroad

--- lib/main.dart
+++ lib/main.dart
@@ -13,84 +13,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text("Stateful Widget Lifecycle")),
-        body: const LifecycleDemo(),
+        appBar: AppBar(title: Text("Dashbroad".toLowerCase()),
+        backgroundColor: Colors.blueGrey,
+        ),
+        body: const Center(
+          child: Text.rich(
+            TextSpan(
+              text: "My",
+                  children:[
+                    TextSpan(
+                    text: "Flutter",
+                      style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
+                    TextSpan(
+                      text: "App",
+                      style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold)
+                    )
+                  ]
+            ),
+          ),
+        ),
       ),
     );
   }
 }
 
-class LifecycleDemo extends StatefulWidget {
-  const LifecycleDemo({super.key});
-
-  @override
-  _LifecycleDemoState createState() {
-    print("createState: Creating the state object");
-    return _LifecycleDemoState();
-  }
-}
-
-class _LifecycleDemoState extends State<LifecycleDemo> {
-  int _counter = 0; // Counter variable
-
-  @override
-  void initState() {
-    super.initState();
-    print("initState: State object initialized");
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print("didChangeDependencies: Dependencies changed");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print("build: Building the widget");
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Stateful Widget Lifecycle",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          Text("Button Pressed: $_counter  times",
-          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              print("Button Pressed: Calling setState");
-              setState(() {
-                _counter++;
-              });
-            },
-            child: const Text("Press Me"),
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  void didUpdateWidget(covariant LifecycleDemo oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    print("didUpdateWidget: Widget updated");
-  }
-
-  @override
-  void deactivate() {
-    super.deactivate();
-    print("deactivate: Widget removed temporarily");
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print("dispose: Cleaning up resources");
-  }
-}
 
 
 
