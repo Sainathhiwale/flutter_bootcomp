1723ebf7f7e25d034b1a53fc5a38864a27aa1e90
Sainathhiwale <sainathhiwale2@gmail.com>
2024-12-16 00:14:02 +0530

    create function return int and string value to the column textviesw to bind data

--- lib/screen/dashbroad.dart
+++ lib/screen/dashbroad.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Dashbroad extends StatelessWidget {
   const Dashbroad({super.key});
@@ -10,18 +13,46 @@ class Dashbroad extends StatelessWidget {
         title: Text("Dashbroad".toLowerCase()),
         backgroundColor: Colors.blueGrey,
       ),
-      body: const Center(
-        child: Text.rich(
-          TextSpan(text: "My", children: [
-            TextSpan(
-                text: "Flutter",
-                style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
-            TextSpan(
-                text: "App",
-                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold))
-          ]),
-        ),
+      body:  Center(
+           child: Column(
+             children: [
+               Text("Random number is ${getNumber()}"),
+               Text("Name is  ${getNameList(2)}"),
+               Text("Random number is ${getNumber()}"),
+             ],
+           )
+
       ),
     );
   }
 }
+
+int getNumber() {
+  return Random().nextInt(100);
+}
+List<String> getNameList(int count) {
+  // Predefined list of strings
+  List<String> names = [
+    "Alice",
+    "Bob",
+    "Charlie",
+    "David",
+    "Eve",
+    "Frank",
+    "Grace",
+    "Hannah",
+    "Ivy",
+    "Jack"
+  ];
+  // List to store random strings
+  List<String> randomNames = [];
+  Random random = Random();
+
+  // Generate 'count' random names
+  for (int i = 0; i < count; i++) {
+    String randomName = names[random.nextInt(names.length)];
+    randomNames.add(randomName);
+  }
+  return randomNames;
+}
+
