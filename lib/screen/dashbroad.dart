c456abdb53ec98e154e8882953a33346339b974a
Sainathhiwale <sainathhiwale2@gmail.com>
2024-12-17 00:40:17 +0530

    icon app is implemented with font_awesome_flutter libs

--- lib/screen/dashbroad.dart
+++ lib/screen/dashbroad.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Dashbroad extends StatelessWidget {
   const Dashbroad({super.key});
@@ -13,26 +14,16 @@ class Dashbroad extends StatelessWidget {
         title: Text("Dashbroad".toLowerCase()),
         backgroundColor: Colors.blueGrey,
       ),
-      body:  Center(
-           child: GridView.count(
-             crossAxisCount: 2, // Number of columns
-             crossAxisSpacing: 10.0, // Spacing between columns
-             mainAxisSpacing: 10.0, // Spacing between rows
-             children: [
-               Image.asset("images/apple.png"),
-               Image.asset("images/casino.png"),
-               Image.asset("images/lottery.png"),
-               Image.asset("images/roulette.png"),
-               Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Tesla_Model_S_Japan_trimmed.jpg/435px-Tesla_Model_S_Japan_trimmed.jpg"),
-               Image.network("https://media.autoexpress.co.uk/image/private/s--3XzRB3yS--/v1565798347/autoexpress/2019/08/04_7.jpg"),
-               Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Tesla_Model_S_Japan_trimmed.jpg/435px-Tesla_Model_S_Japan_trimmed.jpg"),
-               Image.network("https://media.autoexpress.co.uk/image/private/s--3XzRB3yS--/v1565798347/autoexpress/2019/08/04_7.jpg"),
-
-             ],
-           ),
+      body: Center(
+        child: IconButton(
+          icon: Icon(FontAwesomeIcons.arrowLeft),
+          onPressed: () {},
+          color: Colors.cyanAccent,
+          iconSize: 100,
+          splashColor: Colors.white,
+          highlightColor: Colors.greenAccent,
+        ),
       ),
     );
   }
 }
-
-
