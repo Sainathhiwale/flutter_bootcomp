3ef4da9d2e6b3a1abc2225309398c91e5c01c3ce
Sainathhiwale <sainathhiwale2@gmail.com>
2024-12-24 23:55:54 +0530

    listView with listtile is implemented static list item with onTap event and images

--- lib/screen/HomeListViewTile.dart
+++ lib/screen/HomeListViewTile.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Homelistviewtile extends StatelessWidget {
   const Homelistviewtile({super.key});
@@ -6,11 +8,43 @@ class Homelistviewtile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("ListView"),),
       body: Container(
         child: ListView(
+          itemExtent: 100.0,
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
           children: [
-
+            ListTile(
+              leading: CircleAvatar(child: Icon(Icons.person_2_rounded),backgroundColor: Colors.deepPurple.shade300),
+              title: Text("Sale"),
+              subtitle: Text("sale is open"),
+              trailing: Icon(Icons.add),
+              tileColor: Colors.brown.shade50,
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.cabin_sharp),
+              title: Text("Customer"),
+              subtitle: Text("Total Customer Visited"),
+              trailing: Text("500"),
+              tileColor: Colors.deepPurple.shade50,
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.production_quantity_limits_sharp),
+              title: Text("Profit"),
+              subtitle: Text("Profit of week"),
+              trailing: Icon(Icons.alarm_add_sharp),
+              tileColor: Colors.brown.shade50,
+              onTap: (){
+                Fluttertoast.showToast(msg: "Profit is 50%",
+                toastLength: Toast.LENGTH_SHORT,
+                  fontSize: 20.0,
+                  backgroundColor: Colors.blue,
+                  gravity:ToastGravity.CENTER
+                );
+              },
+            ),
           ],
         ),
       ),
