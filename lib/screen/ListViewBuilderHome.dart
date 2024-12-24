bdce34d1253138007bb8aa04a61f496d2a9c64ed
Sainathhiwale <sainathhiwale2@gmail.com>
2024-12-25 01:57:12 +0530

    listview builder is implemented for dynamic records and bind in listtile widgets

--- lib/screen/ListViewBuilderHome.dart
+++ lib/screen/ListViewBuilderHome.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 
 class Listviewbuilderhome extends StatelessWidget {
-  const Listviewbuilderhome({super.key});
+   Listviewbuilderhome({super.key}){
+   for (int i = 4; i <= 100; i++) {
+   product.add("Chair $i");
+   productDetails.add("King Sizw Bad $i");
+   prize.add(i * 10);
+   }
+}
+
+  List<String> product = ["Bad","Safe","Chair"];
+  List<String> productDetails = ["King Sizw Bad","King Size Safe","King Size Chair"];
+  List<int> prize = [200,400,600];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("List View Builder"),),
+     // appBar: AppBar(title: Text("List View Builder"),),
       body: Container(
         child: ListView.builder(
+            itemCount: product.length,
             itemBuilder: (context,index){
-              return Text(index.toString());
+             return ListTile(
+                leading: CircleAvatar(child: Text(product[index][0]),),
+                title: Text(product[index]),
+                subtitle: Text(productDetails[index]),
+                trailing: Text(prize[index].toString()),
+                onTap: (){
+                  Fluttertoast.showToast(msg: product[index],
+                  toastLength: Toast.LENGTH_SHORT,
+                  textColor: Colors.deepPurple,
+                    gravity: ToastGravity.CENTER
+                  );
+                },
+              );
             }
         ),
       ),
