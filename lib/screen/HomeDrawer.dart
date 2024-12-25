005bc533985c7f46f26337aac49f9dc9b3541329
Sainathhiwale <sainathhiwale2@gmail.com>
2024-12-25 23:44:41 +0530

    drawer is implemented with g-mail

--- lib/screen/HomeDrawer.dart
+++ lib/screen/HomeDrawer.dart
@@ -14,21 +14,50 @@ class Homedrawer extends StatelessWidget {
         backgroundColor: Colors.blueAccent,
       ),
       drawer: Drawer(
-        elevation: 0,
         child: ListView(
           children: [
-            DrawerHeader(
-              padding: EdgeInsets.all(0),
-              child: Container(
-                color: Colors.blue,
-                child: Column(
-                  children: [Text("Hi")],
-                ),
-              ),
+            UserAccountsDrawerHeader(
+              accountEmail: Text("sainathhiwale@gmail.com"),
+              accountName: Text("Examen with Sainath"),
+              currentAccountPicture: CircleAvatar(foregroundImage: AssetImage("images/profile.jpg"),),
+              otherAccountsPictures: [
+                CircleAvatar(foregroundImage: AssetImage("images/profile.jpg"),),
+                CircleAvatar(foregroundImage: AssetImage("images/profile.jpg"),),
+              ],
             ),
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Text("All Indexes"),
+            ),
+            ListTile(leading: Icon(Icons.laptop), title: Text("Primary"),onTap: (){},),
+            ListTile(leading: Icon(Icons.label), title: Text("Promoations"),onTap: (){},),
             ListTile(leading: Icon(Icons.home), title: Text("Home"),onTap: (){},),
             ListTile(leading: Icon(Icons.shop), title: Text("Shop"),onTap: (){},),
             ListTile(leading: Icon(Icons.favorite), title: Text("Favorites"),onTap: (){},),
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Text("All Lable"),
+            ),
+            ListTile(leading: Icon(Icons.email), title: Text("Gmail"),onTap: (){},),
+            ListTile(leading: Icon(Icons.star_border), title: Text("Starred"),onTap: (){},),
+            ListTile(leading: Icon(Icons.watch_later_sharp), title: Text("Snoozed"),onTap: (){},),
+            ListTile(leading: Icon(Icons.label), title: Text("Important"),onTap: (){},),
+            ListTile(leading: Icon(Icons.send), title: Text("Send"),onTap: (){},),
+            ListTile(leading: Icon(Icons.schedule_send), title: Text("Scheduled"),onTap: (){},),
+            ListTile(leading: Icon(Icons.outbond_outlined), title: Text("Outbox"),onTap: (){},),
+            ListTile(leading: Icon(Icons.drafts), title: Text("Drafts"),onTap: (){},),
+            ListTile(leading: Icon(Icons.all_inbox), title: Text(" All Mail"),onTap: (){},),
+            ListTile(leading: Icon(Icons.question_mark_sharp), title: Text(" Spam"),onTap: (){},),
+            ListTile(leading: Icon(Icons.delete), title: Text("Bin"),onTap: (){},),
+
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Text("Google apps"),
+            ),
+            ListTile(leading: Icon(Icons.delete), title: Text("Bin"),onTap: (){},),
+            ListTile(leading: Icon(Icons.settings), title: Text("Settings"),onTap: (){},),
+            ListTile(leading: Icon(Icons.help), title: Text("Help"),onTap: (){},),
+
           ],
         ),
       ),
