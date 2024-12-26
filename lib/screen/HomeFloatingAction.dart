0c232b4fb8a1fd2cdf7b9ef8a09b2e5d36ccaaa0
Sainathhiwale <sainathhiwale2@gmail.com>
2024-12-26 12:09:18 +0530

    floating action button with bottomnavigation app bar is implemented notchmargin for floating button to notch with bottomappbar.

--- lib/screen/HomeFloatingAction.dart
+++ lib/screen/HomeFloatingAction.dart
@@ -6,25 +6,32 @@ class Homefloatingaction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+         appBar: AppBar(title: Text("Floating Action Button"),
+           backgroundColor: Colors.blue,
+           titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+         ),
+         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
          floatingActionButton: FloatingActionButton(
            onPressed: (){
            },
            child: Icon(Icons.add),
            backgroundColor: Colors.black87,
            foregroundColor: Colors.yellow,
+           shape: CircleBorder(),
            /*mini: true,
            shape: BeveledRectangleBorder(
              borderRadius: BorderRadius.circular(20.0),
              side: BorderSide(color: Colors.blue,width: 2.0,style: BorderStyle.solid)
            ),*/
            ),
+
          bottomNavigationBar: BottomAppBar(
-           notchMargin: 5.0,
+           notchMargin: 8.0,
            shape: CircularNotchedRectangle(),
            color: Colors.black87,
            child: Row(
              mainAxisAlignment: MainAxisAlignment.spaceAround,
+             mainAxisSize: MainAxisSize.max,
              children: [
                Padding(padding: EdgeInsets.only(left: 10.0),
                  child: Column(
