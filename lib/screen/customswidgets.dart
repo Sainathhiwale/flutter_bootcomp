d331c28f4dbf2640e89d3d0d5e73981f5f8541ca
Sainathhiwale <sainathhiwale2@gmail.com>
2025-02-12 20:07:06 +0530

    make separate widget class for listview to use as custom widget

--- lib/screen/customswidgets.dart
+++ lib/screen/customswidgets.dart
@@ -13,24 +13,46 @@ class Customswidgets extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              tileColor: Colors.black12,
-              shape: ContinuousRectangleBorder(
-               borderRadius: BorderRadius.circular(10.0),
-                side: BorderSide(
-                 width: 1.0,
-                  color: Colors.blue
-                )
-              ),
-              title: Text("Mouse", style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.blue),),
-              subtitle: Text("4TechCom"),
-              leading: IconButton(icon: Icon(Icons.add), onPressed: () {  },),
-              trailing: IconButton(icon: Icon(Icons.ice_skating) , onPressed: () {  },),
-            ),
-          )
+          ListTileWidget(),
+          ListTileWidget(),
+          ListTileWidget()
+
         ],
+      ),
+    );
+  }
+}
+
+class ListTileWidget extends StatelessWidget {
+  const ListTileWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ListTile(
+        tileColor: Colors.black12,
+        shape: ContinuousRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            side: BorderSide(width: 1.0, color: Colors.blue)),
+        title: Text(
+          "Mouse",
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue),
+        ),
+        subtitle: Text("4TechCom"),
+        leading: IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {},
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.ice_skating),
+          onPressed: () {},
+        ),
       ),
     );
   }
