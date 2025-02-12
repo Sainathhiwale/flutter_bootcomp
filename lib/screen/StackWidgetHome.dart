e9c2f7acd14b4801dd3e7221a65988ec4130069f
Sainathhiwale <sainathhiwale2@gmail.com>
2025-02-12 15:15:02 +0530

    created stack widget with Positioned widget to change the stack position

--- lib/screen/StackWidgetHome.dart
+++ lib/screen/StackWidgetHome.dart
@@ -5,6 +5,41 @@ class StackWidgetHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+           title: Text("Stack"),
+           centerTitle: true,
+        backgroundColor: Colors.black26,
+      ),
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Stack(
+         alignment: Alignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 200,
+              color: Colors.green,
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                width: 200,
+                height: 100,
+                color: Colors.yellow,
+              ),
+            ),
+            Positioned(
+              right: 50,
+              child: Container(
+                width: 100,
+                height: 50,
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
