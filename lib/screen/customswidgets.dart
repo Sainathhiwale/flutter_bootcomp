39b513329a127721ae205d3d8221b54fbd110795
Sainathhiwale <sainathhiwale2@gmail.com>
2025-02-12 20:00:19 +0530

    listview with listile widget is added

---
+++ lib/screen/customswidgets.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Customswidgets extends StatelessWidget {
+  const Customswidgets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Customs Widget"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              tileColor: Colors.black12,
+              shape: ContinuousRectangleBorder(
+               borderRadius: BorderRadius.circular(10.0),
+                side: BorderSide(
+                 width: 1.0,
+                  color: Colors.blue
+                )
+              ),
+              title: Text("Mouse", style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.blue),),
+              subtitle: Text("4TechCom"),
+              leading: IconButton(icon: Icon(Icons.add), onPressed: () {  },),
+              trailing: IconButton(icon: Icon(Icons.ice_skating) , onPressed: () {  },),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
