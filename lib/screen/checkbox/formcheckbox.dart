d6116fe9c8a49edfe7901bebc60e9cacecf062f4
Sainathhiwale <sainathhiwale2@gmail.com>
2025-02-16 12:05:20 +0530

    details screen added check box and pass the formcheckbox textfield data to details screen

--- lib/screen/checkbox/formcheckbox.dart
+++ lib/screen/checkbox/formcheckbox.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bootcamp_2025/screen/checkbox/DetailsCheckbox.dart';
 import 'package:flutter_bootcamp_2025/screen/checkbox/mytextfield.dart';
 
 class Formcheckbox extends StatefulWidget {
-   Formcheckbox({super.key});
+  Formcheckbox({super.key});
 
   @override
   State<Formcheckbox> createState() => _FormcheckboxState();
@@ -22,51 +23,73 @@ class _FormcheckboxState extends State<Formcheckbox> {
         centerTitle: true,
       ),
       body: Container(
-          padding: EdgeInsets.all(12.0),
-          child: ListView(
+        padding: EdgeInsets.all(12.0),
+        child: ListView(
           children: [
             Text("Add Product"),
             Text("Add product details in form below"),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
             Mytextfield(
               myController: _productConstroller,
               fieldName: "Product Name",
               myIcon: Icons.propane_outlined,
               prefixiconColor: Colors.deepPurple.shade300,
             ),
-            SizedBox(height: 10.0,),
+            SizedBox(
+              height: 10.0,
+            ),
             Mytextfield(
               myController: _productDetailsConstroller,
               fieldName: "Product Details",
               myIcon: Icons.description_outlined,
               prefixiconColor: Colors.deepPurple.shade300,
             ),
-            SizedBox(height: 10.0,),
-            Checkbox(value: _checkbox, onChanged: (val){
-             setState((){
-                _checkbox = val;
-             });
-            }),
-            SizedBox(height: 10.0,),
-           // Checkbox(value: value, onChanged: onChanged)
+            SizedBox(
+              height: 10.0,
+            ),
+            Checkbox(
+                checkColor: Colors.white,
+                activeColor: Colors.purple,
+                tristate:true ,
+                value: _checkbox,
+                onChanged: (val) {
+                  setState(() {
+                    _checkbox = val;
+                  });
+                }),
+            SizedBox(
+              height: 10.0,
+            ),
+            // Checkbox(value: value, onChanged: onChanged)
             myBtn(context),
-        ],
+          ],
+        ),
       ),
-    ),);
+    );
   }
 
-   OutlinedButton myBtn(BuildContext context) {
-     return OutlinedButton(
-       style: OutlinedButton.styleFrom(minimumSize: const Size(200, 50),
-           backgroundColor: Colors.blue,
-           foregroundColor: Colors.white),
-       onPressed: () {
-       },
-       child: Text(
-         "Submit Form".toUpperCase(),
-         style: const TextStyle(fontWeight: FontWeight.bold),
-
-       ),
-     );
-   }
+  OutlinedButton myBtn(BuildContext context) {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+          minimumSize: const Size(200, 50),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return Detailscheckbox(producName: _productConstroller.text, productDetails: _productDetailsConstroller.text, );
+            },
+          ),
+        );
+      },
+      child: Text(
+        "Submit Form".toUpperCase(),
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
 }
