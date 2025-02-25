43872d0c1bfdc8eeae3aac2e4bc199730cb343c6
Sainathhiwale <sainathhiwale2@gmail.com>
2025-02-26 00:33:52 +0530

    dropdown is designed with list of string and click listener and some icon with underlineinputborder

--- lib/screen/dropdown_spineer/form_dropdown.dart
+++ lib/screen/dropdown_spineer/form_dropdown.dart
@@ -3,17 +3,24 @@ import 'package:flutter_bootcamp_2025/screen/custome_widgets/mytextfield.dart';
 import 'package:flutter_bootcamp_2025/screen/dropdown_spineer/details_dropdown.dart';
 
 class FormDropdown extends StatefulWidget {
-  const FormDropdown({super.key});
+   const FormDropdown({super.key});
 
   @override
   State<FormDropdown> createState() => _FormDropdownState();
 }
 
 class _FormDropdownState extends State<FormDropdown> {
+  _FormDropdownState(){
+  _selectedVal = _productsizeList[0];
+  }
   final _productConstroller = TextEditingController();
   final _productDetailsConstroller = TextEditingController();
   bool? _checkbox = false;
   bool? _checkboxlistTile = false;
+
+  final _productsizeList = ["Small","Medium","Large","Xlarge"];
+  String? _selectedVal ="";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,7 +80,33 @@ class _FormDropdownState extends State<FormDropdown> {
             SizedBox(
               height: 20.0,
             ),
-            // Checkbox(value: value, onChanged: onChanged)
+            DropdownButtonFormField(
+                value: _selectedVal,
+                items: _productsizeList.map(
+                (e) => DropdownMenuItem(child: Text(e), value: e,)
+            ).toList(),
+                onChanged: (val){
+                  setState(() {
+                    _selectedVal = val as String;
+                  });
+            },
+              icon: const Icon(
+                Icons.arrow_drop_down_circle,
+                color: Colors.deepPurple,
+              ),
+              dropdownColor:Colors.deepPurple.shade50 ,
+              decoration: InputDecoration(
+                labelText: "Product Size",
+                prefixIcon: Icon(
+                    Icons.accessibility_sharp,
+                  color: Colors.deepPurple,
+                ),
+                border: UnderlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
             myBtn(context),
           ],
         ),
